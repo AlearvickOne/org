29a4329da6675d5362025f116fd65b7c33f17fa9
AlearvickOne <Alexandr73.73.73.73@mail.ru>
2025-01-12 21:48:14 +0300

    Блог админка ч2

---
+++ migrations/1736702376738-create-table-blogs.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableBlogs1736702376738 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`Create table blogs (
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    photo VARCHAR(255),
+    content LONGTEXT
+  )`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
